d88fdbf9ebd10d2fe2f61a23dea5a0fddab51867
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-03-30 10:15:12 -0700

    hide results with CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
+
+
 $(document).ready(function() {
+$("form#")
   var animal = ["", "", ""];
   var child = ["", "", ""];
   var adult = ["", "", ""];
   var yourFavorites = [animal, child, adult];
 })
+
+
+var question1 = $("input:radio[name=q1]:checked").val();
