fecf20e10ed283d64be5e7e175d39d343fddf740
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-03-30 11:00:18 -0700

    grab user data from DOM and form an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
 
 
 $(document).ready(function() {
-$("form#")
-  var animal = ["", "", ""];
-  var child = ["", "", ""];
-  var adult = ["", "", ""];
-  var yourFavorites = [animal, child, adult];
-})
+  $("form#fav").submit(function(event){
+    event.preventDefault();
+    // Grab user data from DOM and store it in variables
+    var animal = $('.animal').val();
+    var child = $('.child').val();
+    var adult = $(".adult").val();
+    // console.log("animal: " + animal);
+    // console.log("child: " + child);
+    // console.log("adult: " + adult);
+    // Console.log() those vars
 
+    // Put them into an array
+     var yourFavorites = [".animal", ".child", ".adult"];
+    // console.log(yourFavorites);
+     
+     // display them on the DOM
+  });
+});
 
-var question1 = $("input:radio[name=q1]:checked").val();
